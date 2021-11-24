5954afdbe9018e2f2432bea3ff71213f3245e54f
allencode11 <66513253+allencode11@users.noreply.github.com>
2021-11-24 23:11:05 +0200

    Update index.py

--- server/index.py
+++ server/index.py
@@ -1,16 +1,26 @@
-from flask import Flask, jsonify
+from PIL import Image
+import torchvision.transforms as transforms
+from torchvision import models
 import io
 import json
 import torch
 from torch import nn as nn
+import os
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-from torchvision import models
-
-import torchvision.transforms as transforms
-from PIL import Image
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 model = models.resnet50(pretrained=False)
@@ -23,8 +33,7 @@ model.fc = nn.Sequential(
     nn.Linear(32, 3),
     nn.LogSoftmax()
 )
-model.load_state_dict(torch.load(
-    './models/covid19/covid19.pt', map_location='cpu'))
+model.load_state_dict(torch.load('./covid19.pt', map_location='cpu'))
 
 model.eval()
 
@@ -46,17 +55,38 @@ def transform_image(image_bytes):
             std=[0.229, 0.224, 0.225]
         )
     ])
-    image = Image.open(io.BytesIO(image_bytes)).convret('RGB')
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
     return my_transforms(image).unsqueeze(0)
 
 
 @app.route('/')
 def hello():
-    with open("./images/image3.jpg", 'rb') as f:
+    with open("./images/0103.jpeg", 'rb') as f:
         image_bytes = f.read()
         print(get_prediction(image_bytes=image_bytes))
 
     return 'Hello World!'
+
+
+@app.route('/api/image/upload', methods=['POST'])
+def uploadImage():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        print(file)
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return jsonify()
 
 
 @app.route('/predict', methods=['POST'])
